e5b8de50a654c101edd92e1175b99f0438e4856c
alona22193 <alona22193@gmail.com>
2017-10-22 16:24:16 -0400

    created about page

--- Website Files/js/script.js
+++ Website Files/js/script.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
 
 
   	var scroll = $(window).scrollTop();
-	  if (scroll > 160) {
-	    $(".navCustom").css("background" , "-webkit-gradient(linear,left top,left bottom,from(#36414F),to(#364F4C))");
+	  if (scroll > 200) {
+	    $(".navCustom").css("background" , "-webkit-gradient(linear,left top,left bottom,from(#0d505a),to(#106470))");
 	  }
 
 	  else{
