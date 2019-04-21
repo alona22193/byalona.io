3675778c0234fb76eb64fb69bbd60b8488e5d48d
alona22193 <alona22193@gmail.com>
2019-04-20 21:13:57 -0400

    Added mobile hamburger menu to shake shop website

--- Website Files/portfolio/shakeshop/js/main.js
+++ Website Files/portfolio/shakeshop/js/main.js
@@ -16,3 +16,13 @@ function arrow() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//Hamburger Mobile Menu
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
